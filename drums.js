fbc6f0431e72496accdcdf97cef07fd587230870
Nigel Marshal <nigel566@hotmail.com>
2021-09-21 23:20:00 +0400

    fix(js): Fixed class toggle and transform element fast tapping

--- drums.js
+++ drums.js
@@ -1,7 +1,22 @@
-window.addEventListener("keydown", function (e) {
-  console.log(e.keyCode);
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
-  if (!audio) return; //Stop function is audio for the element is not present
-  audio.play();
-});
+function playSound(e) {
+  {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return; //Stop function is audio for the element is not present
+    audio.currentTime = 0; //Prevent delay when spamming keys
+    audio.play();
+    audio.volume = 0.2;
+    key.classList.add("playing");
+  }
+}
+
+function removeTransition(e) {
+  //Skip if not transform
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
